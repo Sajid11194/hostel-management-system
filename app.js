5b12f9ce964ad2187323028a865db93fadac46cd
Farhan Sajid <fsreg11194@gmail.com>
2022-10-08 21:36:57 +0600

    just saving the changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require("passport");
 const MongoDBStore = require('connect-mongodb-session')(session);
 const LocalStrategy = require("passport-local");
 const app = express();
+const slashes = require("connect-slashes");
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -38,86 +39,6 @@ const adminSchema = new mongoose.Schema({
     }
 })
 const Admin = new mongoose.model("Admin", adminSchema);
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-passport.use('admin', new LocalStrategy((username, password, callback) => {
-    Admin.findOne({username: username, password: password}, (err, admin) => {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null, admin);
-        }
-    });
-}));
-passport.use('user', new LocalStrategy((username, password, callback) => {
-    User.findOne({username: username, password: password}, (err, user) => {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null, user);
-        }
-    });
-}));
-
-passport.serializeUser(function (user, done) {
-    let role = "user";
-    // if(user.isAdmin){
-    //     isAdmin=true;
-    // }
-    done(null, {_id: user._id, role: role});
-});
-
-passport.deserializeUser(function (serializedUser, callback) {
-    process.nextTick(function () {
-        let target = User;
-        if (serializedUser.role == "admin") {
-            target = Admin;
-        }
-        target.findById(serializedUser._id, function (err, user) {
-            console.log("FOUND :")
-            console.log(user)
-            callback(null, user);
-        });
-    });
-
-});
-
-function isUser(req) {
-    return req.session.passport.user.role == "user";
-}
-
-function isAdmin(req) {
-    return req.session.passport.user.role == "admin";
-}
-
-const seatSchema = new mongoose.Schema({
-    seatName: {
-        type: String,
-        required: true,
-        unique: true,
-        uppercase: true
-    },
-    roomName: {
-        type: String,
-        required: true,
-        uppercase: true
-    },
-    hostelName: {
-        type: String,
-        required: true,
-        unique: true,
-        uppercase: true
-    },
-    onService: {
-        type: Boolean,
-        default: true
-    },
-    resident: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
-})
-
-const Seat = mongoose.model("Seat", seatSchema);
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -155,6 +76,100 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model("User", userSchema);
+const roomSchema= new mongoose.Schema({
+    roomName:String,
+    seats:[{type: mongoose.Schema.Types.ObjectId, ref: 'Seat'}]
+})
+const hostelSchema = new mongoose.Schema({
+    hostelName:String,
+    address:String,
+    floors:Number,
+    rooms:[roomSchema]
+})
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use('admin', new LocalStrategy((username, password, callback) => {
+    Admin.findOne({username: username, password: password}, (err, user) => {
+        if (err) {
+            callback(err);
+        } else {
+            newAdmin = {
+                role: "admin",
+                user: user
+            }
+            callback(null, newAdmin);
+        }
+    });
+}));
+passport.use('user', new LocalStrategy((username, password, callback) => {
+    User.findOne({username: username, password: password}, (err, user) => {
+        if (err) {
+            callback(err);
+        } else {
+            newUser = {
+                role: "user",
+                user
+            }
+            callback(null, newUser);
+        }
+    });
+}));
+
+passport.serializeUser(function (user, done) {
+    done(null, {_id: user.user._id, role: user.role});
+});
+
+passport.deserializeUser(function (serializedUser, callback) {
+    process.nextTick(function () {
+        let target = User;
+        if (serializedUser.role == "admin") {
+            target = Admin;
+        }
+        target.findById(serializedUser._id, function (err, user) {
+
+            callback(null, user);
+        });
+    });
+
+});
+
+function isUser(req) {
+    return req.isAuthenticated() && (req.session.passport.user.role == "user");
+}
+
+function isAdmin(req) {
+    return req.isAuthenticated() && (req.session.passport.user.role == "admin");
+}
+
+const seatSchema = new mongoose.Schema({
+    seatName: {
+        type: String,
+        required: true,
+        unique: true,
+        uppercase: true
+    },
+    roomName: {
+        type: String,
+        required: true,
+        uppercase: true
+    },
+    hostelName: {
+        type: String,
+        required: true,
+        unique: true,
+        uppercase: true
+    },
+    onService: {
+        type: Boolean,
+        default: true
+    },
+    resident: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+})
+
+const Seat = mongoose.model("Seat", seatSchema);
+
 const applicationSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
@@ -182,20 +197,35 @@ const applicationSchema = new mongoose.Schema({
 
 })
 const Application = mongoose.model("Application", applicationSchema);
-// app.use(/^[\/]admin($|\/).*/,(req,res,next)=>{
-//     if(isAdmin()){
-//         next();
-//     }
-//     else{
-//         res.redirect("/admin/login")
-//     }
-// })
-app.get("/", (req, res) => {
-    if (req.isAuthenticated()) {
-        res.send(req.session);
+app.use((req, res, next) => {
+    app.locals.global = {
+        role: req.session.passport?.user.role,
+        user: req?.user,
+        path:req.path
+    };
+
+    next()
+})
+app.use(/^[\/]admin($|\/)(?!login)(?!register).*/, (req, res, next) => {
+    if (isAdmin(req)) {
+        next();
     } else {
-        res.redirect("/user/login");
+        res.redirect("/admin/login")
     }
+})
+
+app.use(/^[\/]user($|\/)(?!login)(?!register).*/, (req, res, next) => {
+    if (isUser(req)) {
+        next();
+    } else {
+        res.redirect("/user/login")
+    }
+})
+
+app.use(slashes(false));
+
+app.get("/", (req, res) => {
+
 });
 
 app.get("/user/login", (req, res) => {
@@ -237,118 +267,126 @@ app.post("/user/register", (req, res) => {
 });
 
 app.get("/user/profile", (req, res) => {
-    if (!isUser(req)) {
-        res.redirect("/user/login")
-    } else {
-        console.log(req.user);
-        res.render('user/profile', {user: req.user});
-    }
+    res.render('user/profile', {user: req.user});
 });
 
 app.post("/user/profile", (req, res) => {
-    if (!isUser(req)) {
-        res.redirect("/user/login")
-    } else {
-        const firstName = req.body.firstName;
-        const lastName = req.body.lastName;
-        const gender = req.body.gender;
-        const dob = req.body.dob;
-        const email = req.body.email;
-        const phoneNumber = req.body.phoneNumber;
-        const permanentAddress = req.body.permanentAddress;
-        User.findByIdAndUpdate(req.user._id, {
-            profile: {
-                firstName,
-                lastName,
-                gender,
-                dob,
-                email,
-                contact: {
-                    phoneNumber,
-                    permanentAddress
-                }
+
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const gender = req.body.gender;
+    const dob = req.body.dob;
+    const email = req.body.email;
+    const phoneNumber = req.body.phoneNumber;
+    const permanentAddress = req.body.permanentAddress;
+    User.findByIdAndUpdate(req.user._id, {
+        profile: {
+            firstName,
+            lastName,
+            gender,
+            dob,
+            email,
+            contact: {
+                phoneNumber,
+                permanentAddress
             }
-        }, (err, user) => {
-            if (err) {
-                console.log(err)
-            } else {
-                res.redirect("/user/profile")
-            }
-        });
-    }
+        }
+    }, (err, user) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect("/user/profile")
+        }
+    });
+
 });
 
 app.get("/user/apply", (req, res) => {
-    if (!isUser(req)) {
-        res.redirect("/user/login")
-    } else {
-        res.render('user/apply', {user: req.user});
-    }
+
+    res.render('user/apply', {user: req.user});
+
 });
 
 app.post("/user/apply", (req, res) => {
-    if (!isUser(req)) {
-        res.redirect("/user/login")
-    } else {
-        const hostelName = req.body.hostelName;
-        const roomName = req.body.roomName;
-        const package = req.body.package;
-        const method = req.body.paymentMethod;
-        const amount = req.body.paymentAmount;
-        const trxId = req.body.trxId;
-        const note = req.body.note;
-        const applicationDate = new Date();
-        const lastSubmitDate = new Date();
+    const hostelName = req.body.hostelName;
+    const roomName = req.body.roomName;
+    const package = req.body.package;
+    const method = req.body.paymentMethod;
+    const amount = req.body.paymentAmount;
+    const trxId = req.body.trxId;
+    const note = req.body.note;
+    const applicationDate = new Date();
+    const lastSubmitDate = new Date();
 
-        Application.create({
-            user: req.user._id,
-            hostel: {
-                hostelName,
-                roomName
-            },
-            package,
-            payment: {
-                method,
-                amount,
-                trxId
-            },
-            applicationDate,
-            lastSubmitDate,
-            note
-        }, (err, application) => {
-            if (err) {
+    Application.create({
+        user: req.user._id,
+        hostel: {
+            hostelName,
+            roomName
+        },
+        package,
+        payment: {
+            method,
+            amount,
+            trxId
+        },
+        applicationDate,
+        lastSubmitDate,
+        note
+    }, (err, application) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(application)
+            User.findByIdAndUpdate(req.user._id, {$push: {applications: application._id}}, (err, updatedUserApplications) => {
                 console.log(err)
-            } else {
-                console.log(application)
-                User.findByIdAndUpdate(req.user._id, {$push: {applications: application._id}}, (err, updatedUserApplications) => {
-                    console.log(err)
-                    console.log("updatedUserApplications")
-                    console.log(updatedUserApplications)
-                    res.redirect("/")
-                })
+                console.log("updatedUserApplications")
+                console.log(updatedUserApplications)
+                res.redirect("/")
+            })
 
-            }
-        });
-    }
+        }
+    });
+
 });
 
 app.listen(80, () => {
     console.log("Server Started");
 });
 
+app.get("/admin", (req, res) => {
+    //testing if running query in parallel will reduce time or not
 
-app.get("/admin/login", (req, res) => {
-    res.render('admin/login');
+    Promise.all([Application.find({status:"pending"}).sort({_1:1}).limit(5).populate('user'),Application.count(),User.count(),Seat.count(),Seat.count({resident:null})]).then(([applications,appCount,userCount,seatCount,emptySeatCount])=>{
+        const values={applications,stats:{appCount,userCount,seatCount,emptySeatCount}}
+        res.render('admin/dashboard',values);
+    });
 });
 
-app.post('/admin/login', passport.authenticate('admin', {failureRedirect: '/admin/login'}), (err, req, res, next) => {
+app.get("/admin/login", (req, res) => {
+
+    if (!isAdmin(req)) {
+        res.render('admin/login');
+    } else {
+        res.redirect("/admin");
+    }
+});
+
+app.post('/admin/login', passport.authenticate('admin', {
+    successRedirect: '/admin',
+    failureRedirect: '/admin/login'
+}), (err, req, res, next) => {
     if (err) next(err);
-    res.redirect("/");
+    res.redirect("/admin");
 });
 
 
 app.get("/admin/register", (req, res) => {
-    res.render('admin/register');
+    if (!isAdmin(req)) {
+        res.render('admin/register');
+    } else {
+        res.redirect("/");
+    }
 });
 
 app.post("/admin/register", (req, res) => {
@@ -369,7 +407,7 @@ app.post("/admin/register", (req, res) => {
 });
 
 
-app.get(["/admin/applications/:status", "/admin/applications/"], (req, res) => {
+app.get(["/admin/applications/:status", "/admin/applications"], (req, res) => {
     const status = req.params.status;
     let page = req.query.p;
     if (page < 1 || page == undefined) { //defaults to page 1
@@ -380,6 +418,7 @@ app.get(["/admin/applications/:status", "/admin/applications/"], (req, res) => {
     if (status != "all" && status != undefined) {
         find.status = status; // building query, filter by status only if users wants
     }
+
     //get only 10 results and | if page 2 then skips first (2*10-10)=10 results
     Application.find(find).skip(page * limit - limit).limit(limit).populate('user').exec((err, applications) => {
         if (err) {
@@ -402,14 +441,14 @@ app.get(["/admin/applications/:status", "/admin/applications/"], (req, res) => {
 app.get("/admin/applications/review/:id", (req, res) => {
     const id = req.params.id;
     if (id == undefined) {
-        res.redirect("/admin/applications/");
+        res.redirect("/admin/applications");
     }
     Application.findById(id).populate('user').exec((err, application) => {
-        res.render('admin/review', {application,path:req.path})
+        res.render('admin/review', {application, path: req.path})
     })
 
 });
-app.get("/admin/applications/review/:id/:action",(req,res)=>{
+app.get("/admin/applications/review/:id/:action", (req, res) => {
     console.log("++++++++++++++++++++++++")
 });
 app.post("/admin/applications/review/:id/:action", (req, res) => {
@@ -419,7 +458,8 @@ app.post("/admin/applications/review/:id/:action", (req, res) => {
     const message = req.body.message;
     const seatName = req.body.seatName;
     const package = req.body.package;
-//{status:action,lastSubmitDate:new Date(),noteFromAdmin:message},
+
+//here comes nested callback cancer :p
     if (applicationAction == "accepted") {
         Application.findById(applicationId).populate('user').exec((errApp, application) => {
             Seat.findOne({seatName}, (seatError, seat) => {
@@ -429,19 +469,22 @@ app.post("/admin/applications/review/:id/:action", (req, res) => {
                             if (seatUpdateError) {
                                 console.log("//seat update error");
                             } else {
-                                User.findByIdAndUpdate(application.user._id, {seat: seat._id,package:package}, (errUser, user) => {
+                                User.findByIdAndUpdate(application.user._id, {
+                                    seat: seat._id,
+                                    package: package
+                                }, (errUser, user) => {
                                     if (errUser) {
                                         console.log("//user update error");
                                     } else {
                                         application.update({
                                             status: applicationAction,
-                                            lastSubmitDate:new Date(),
+                                            lastSubmitDate: new Date(),
                                             noteFromAdmin: message
                                         }, (appUpdateError, updatedApp) => {
                                             if (appUpdateError) {
                                                 console.log("//app update error");
                                             } else {
-                                                res.redirect("/admin/applications/")
+                                                res.redirect("/admin/applications")
                                             }
                                         })
                                     }
@@ -464,9 +507,10 @@ app.post("/admin/applications/review/:id/:action", (req, res) => {
             if (err) {
                 console.log(err);
             } else {
-                res.redirect("/admin/applications/");
+                res.redirect("/admin/applications");
             }
         })
     }
 
 });
+
--- test2.js
+++ test2.js
@@ -39,22 +39,120 @@ const applicationSchema = new mongoose.Schema({
 
 })
 const Application = mongoose.model("Application", applicationSchema);
-const Test = mongoose.model("Test", applicationSchema);
-
-const info=(xd)=>{
-    return {
-        user: "633c455ddfe915efc9cf44d0", hostel:{hostelName:xd+"  GG", roomName:"GG1A"}, package:"Luxury", payment: {
-        method:"Bkash", amount:"100", trxId:"HH"
-    }, applicationDate:new Date(), lastSubmitDate:new Date(), note:xd,status:"rejected"
-    }
-};
-for(let i=0;i<100;i++){
-Application.create(info(i), (err, application) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(application);
-
-    }
+const roomSchema = new mongoose.Schema({
+    roomName: {
+        type: String,
+        uppercase: true
+    },
+    floor: Number,
+    seats: [{type: mongoose.Schema.Types.ObjectId, ref: 'Seat'}]
 });
-}
+const Room = mongoose.model('Room', roomSchema);
+
+const seatSchema = new mongoose.Schema({
+    seatName: {
+        type: String,
+        required: true,
+        unique: true,
+        uppercase: true
+    },
+    roomName: {
+        type: String,
+        required: true,
+        uppercase: true
+    },
+    hostelName: {
+        type: String,
+        required: true,
+        unique: true,
+        uppercase: true
+    },
+    onService: {
+        type: Boolean,
+        default: true
+    },
+    resident: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+})
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+    },
+    balance: {
+        type: Number,
+        default: 0
+    },
+    profile: {
+        firstName: String,
+        lastName: String,
+        gender: {
+            type: String,
+            enum: ["male", "female"]
+        },
+        dob: String,
+        contact: {
+            phoneNumber: String,
+            permanentAddress: String
+        },
+    },
+    seat: {type: mongoose.Schema.Types.ObjectId, ref: 'Seat'},
+    applications: [{type: mongoose.Schema.Types.ObjectId, ref: 'Application'}],
+    package: String
+});
+
+const User = mongoose.model("User", userSchema);
+const Seat = mongoose.model("Seat", seatSchema);
+const room = new Room({
+    roomName: "101",
+    floor: 1,
+
+})
+
+const hostelSchema = new mongoose.Schema({
+    hostelName: String,
+    address: String,
+    floors: Number,
+    rooms:[roomSchema]
+});
+const Hostel = mongoose.model('hostellist', hostelSchema);
+
+const hostel = new Hostel({
+    hostelName: "Laal Boys Ashulia",
+    address: "khagan",
+    floors: 150
+});
+
+Hostel.findOne({hostelName:"Laal Boys Ashulia"},(err,res)=>{
+
+    res.rooms.push(room);
+    res.save((e,r)=>{
+        console.log("saved");
+    })
+})
+
+// const info=(xd)=>{
+//     return {
+//         user: "633c455ddfe915efc9cf44d0", hostel:{hostelName:xd+"  GG", roomName:"GG1A"}, package:"Luxury", payment: {
+//         method:"Bkash", amount:"100", trxId:"HH"
+//     }, applicationDate:new Date(), lastSubmitDate:new Date(), note:xd,status:"rejected"
+//     }
+// };
+// for(let i=0;i<100;i++){
+// Application.create(info(i), (err, application) => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log(application);
+//
+//     }
+// });
+// }
